2f4cb0b7106093ae388536f0de7ec95dd71b7619
Nabeel <nabeel.sohail2630@outlook.com>
2022-05-23 19:52:08 +0500

    DB Connection Class

---
+++ PropertyManagementSystem/PropertyManagementSystem/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManagementSystem
+{
+    abstract class DbConnection
+    {
+        private readonly string connectionString;
+
+        public DbConnection()
+        {
+            connectionString = "Server=NABEELS-WORK;Database=PropertyManagementSystem;User Id=PMS;Password=Nabeel30;MultipleActiveResultSets=True;";
+
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
