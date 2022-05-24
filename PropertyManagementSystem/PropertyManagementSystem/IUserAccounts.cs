628e41ed55edb321d155386ae76f57d1eb0bf2b4
Nabeel <nabeel.sohail2630@outlook.com>
2022-05-24 06:06:08 +0500

    UserAccounts Interface Created

---
+++ PropertyManagementSystem/PropertyManagementSystem/IUserAccounts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManagementSystem
+{
+    interface IUserAccounts
+    {
+        int AuthenticateLogin(string pUserName, string pPassword);  //returns UserAccountId
+
+        UserAccountsModel GetUserById(int pUserAccountId);  //returns complete user detail against id
+    }
+}
