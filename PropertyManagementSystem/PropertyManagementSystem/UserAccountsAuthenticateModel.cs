79d5589c8c6444b51da0a08a860daa6cb22ee91b
Nabeel <nabeel.sohail2630@outlook.com>
2022-05-28 01:46:40 +0500

    User Authenticate Model Created

---
+++ PropertyManagementSystem/PropertyManagementSystem/UserAccountsAuthenticateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManagementSystem
+{
+    class UserAccountsAuthenticateModel
+    {
+        public int UserAccountId { get; set; }
+        public bool Active { get; set; }
+        public bool AccountLockedOut { get; set; }
+    }
+}
