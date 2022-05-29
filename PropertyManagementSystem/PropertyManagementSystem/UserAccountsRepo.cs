a23102b8af5fe7f40ae983ace8f674454fc18890
Nabeel <nabeel.sohail2630@outlook.com>
2022-05-30 00:06:29 +0500

    Methods Created

--- PropertyManagementSystem/PropertyManagementSystem/UserAccountsRepo.cs
+++ PropertyManagementSystem/PropertyManagementSystem/UserAccountsRepo.cs
@@ -10,10 +10,13 @@ namespace PropertyManagementSystem
     class UserAccountsRepo : DbConnection, IUserAccounts
     {
         private SqlConnection conn;
+        private SqlCommand cmd;
 
         UserAccountsRepo()
         {
             conn = GetConnection();
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
         }
 
         ~UserAccountsRepo()
@@ -21,11 +24,35 @@ namespace PropertyManagementSystem
             conn.Close();
             conn.Dispose();
             conn = null;
+
+            cmd.Dispose();
+            cmd = null;
         }
 
         public int AddUser(UserAccountsModel user)
         {
-            throw new NotImplementedException();
+            int UserAccountId = 0;
+
+            using (conn)
+            {
+                using (cmd)
+                {
+                    cmd.CommandText = "spAddUserAccount";
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@pUserName", user.UserName);
+                    cmd.Parameters.AddWithValue("@pFirstName", user.FirstName);
+                    cmd.Parameters.AddWithValue("@pLastName", user.LastName);
+                    cmd.Parameters.AddWithValue("@pEmail", user.Email);
+                    cmd.Parameters.AddWithValue("@pUserPassword", user.Password);
+                    cmd.Parameters.AddWithValue("@pMobile", user.Mobile);
+                    cmd.Parameters.AddWithValue("@pTwoFactorAuthentication", user.TwoFactorAuthentication);
+                    cmd.Parameters.AddWithValue("@pAllowMultipleLogin", user.AllowMultipleLogin);
+
+                    UserAccountId = (int)cmd.ExecuteScalar();
+                }
+            }
+
+            return UserAccountId;
         }
 
         public UserAccountsAuthenticateModel AuthenticateLogin(string pUserName, string pPassword)
@@ -34,7 +61,7 @@ namespace PropertyManagementSystem
 
             using (conn)
             {
-                using(SqlCommand cmd = new SqlCommand())
+                using (SqlCommand cmd = new SqlCommand())
                 {
                     cmd.Connection = conn;
                     cmd.CommandText = "spAuthenticateUserAccount";
@@ -48,6 +75,8 @@ namespace PropertyManagementSystem
                     UserAuthenticate.UserAccountId = (int)dr["UserAccountId"];
                     UserAuthenticate.Active = (bool)dr["Active"];
                     UserAuthenticate.AccountLockedOut = (bool)dr["AccountLockedOut"];
+                    UserAuthenticate.IsLogged = (bool)dr["IsLogged"];
+                    UserAuthenticate.AllowMultipleLogin = (bool)dr["AllowMultipleLogin"];
 
                     dr.Close();
                     dr = null;
@@ -57,12 +86,12 @@ namespace PropertyManagementSystem
             return UserAuthenticate;
         }
 
-        public UserAccountsModel GetUserById(int pUserAccountId)
+        public UserAccountsModel Find(int pUserAccountId)
         {
             UserAccountsModel user;
             using (conn)
             {
-                using(SqlCommand cmd = new SqlCommand())
+                using (SqlCommand cmd = new SqlCommand())
                 {
                     cmd.Connection = conn;
                     cmd.CommandText = "spGetUserAccountById";
@@ -77,7 +106,16 @@ namespace PropertyManagementSystem
                     user.UserName = (string)dr["UserName"];
                     user.FirstName = (string)dr["FirstName"];
                     user.LastName = (string)dr["LastName"];
-
+                    user.Email = (string)dr["Email"];
+                    user.Mobile = (string)dr["Mobile"];
+                    user.TwoFactorAuthentication = (bool)dr["TwoFactorAuthentication"];
+                    user.Active = (bool)dr["Active"];
+                    user.AccountLockedOut = (bool)dr["AccountLockedOut"];
+                    user.WrongAttempts = (int)dr["WrongAttempts"];
+                    user.LastLoginDateTime = (DateTime)dr["LastLoginDateTime"];
+                    user.LastLoggedOutDateTime = (DateTime)dr["LastLoggedOutDateTime"];
+                    user.IsLogged = (bool)dr["IsLogged"];
+                    user.AllowMultipleLogin = (bool)dr["AllowMultipleLogin"];
 
                     dr.Close();
                     dr = null;
@@ -87,9 +125,61 @@ namespace PropertyManagementSystem
             return user;
         }
 
-        public void SignOut()
+        public UserAccountsModel Find(string pUserName)
         {
-            throw new NotImplementedException();
+            UserAccountsModel user;
+            using (conn)
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "spGetUserAccountByUserName";
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@pUserName", pUserName);
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    user = new UserAccountsModel();
+
+                    user.UserAccountId = (int)dr["UserAccountId"];
+                    user.UserName = (string)dr["UserName"];
+                    user.FirstName = (string)dr["FirstName"];
+                    user.LastName = (string)dr["LastName"];
+                    user.Email = (string)dr["Email"];
+                    user.Mobile = (string)dr["Mobile"];
+                    user.TwoFactorAuthentication = (bool)dr["TwoFactorAuthentication"];
+                    user.Active = (bool)dr["Active"];
+                    user.AccountLockedOut = (bool)dr["AccountLockedOut"];
+                    user.WrongAttempts = (int)dr["WrongAttempts"];
+                    user.LastLoginDateTime = (DateTime)dr["LastLoginDateTime"];
+                    user.LastLoggedOutDateTime = (DateTime)dr["LastLoggedOutDateTime"];
+                    user.IsLogged = (bool)dr["IsLogged"];
+                    user.AllowMultipleLogin = (bool)dr["AllowMultipleLogin"];
+
+                    dr.Close();
+                    dr = null;
+                }
+            }
+
+            return user;
         }
+
+        public void SignOut(int pUserAccountId)
+        {
+            using (conn)
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "spSignOut";
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@pUserId", pUserAccountId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        
     }
 }
