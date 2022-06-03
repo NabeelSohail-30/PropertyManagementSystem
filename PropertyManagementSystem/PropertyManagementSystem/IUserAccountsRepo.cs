0aac2d986affc9419c11ff06422b4a4b7f3df6bc
Nabeel <nabeel.sohail2630@outlook.com>
2022-06-03 19:10:31 +0500

    Name changed for interface user accounts

---
+++ PropertyManagementSystem/PropertyManagementSystem/IUserAccountsRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManagementSystem
+{
+    public interface IUserAccountsRepo
+    {
+        UserAccountsModel AuthenticateLogin(string pUserName, string pPassword);
+
+        UserAccountsModel Find(int pUserAccountId);  //returns complete user detail against id
+        UserAccountsModel Find(string pUserName);  //returns complete user detail against username
+
+        int AddUser(UserAccountsModel user);
+
+        void SignOut(int UserId);
+    }
+}
