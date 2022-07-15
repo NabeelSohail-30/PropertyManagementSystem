87cd6a58d3f4f49d09fbe6cd5a2521eb87e593e4
Nabeel <nabeel.sohail2630@outlook.com>
2022-07-16 03:23:16 +0500

    Master Page for Main Site

---
+++ PropertyManagementSystem/PropertyManagementSystemWebApp/Master_Main.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PropertyManagementSystemWebApp
+{
+    public partial class Master_Main : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MainMenu.aspx");
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LoginForm.aspx");
+        }
+    }
+}
